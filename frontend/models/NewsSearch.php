c4ed89bd84d952a614e8cf90d3e68cd056c98b42
Hodym <hodimchukdmitriy@gmail.com>
2020-07-16 16:24:17 +0300

    Leson 20

--- frontend/models/NewsSearch.php
+++ frontend/models/NewsSearch.php
@@ -18,7 +18,10 @@ class NewsSearch
     
     public function fulltextSearch($keyword)
     {
-        $sql = "SELECT * FROM news WHERE MATCH (content) AGAINST ('$keyword') LIMIT 20";
-        return Yii::$app->db->createCommand($sql)->queryAll();
+        $params = [
+            ':keyword' => $keyword,
+        ];
+        $sql = "SELECT * FROM news WHERE MATCH (content) AGAINST (:keyword) LIMIT 20";
+        return Yii::$app->db->createCommand($sql)->bindValues($params)->queryAll();
     }
 }
